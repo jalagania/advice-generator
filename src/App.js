004d95fabd003e8f35ca262cc04514d48712e7d6
jalagania <beka>
2022-09-24 13:39:42 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import dice from "./images/icon-dice.svg";
+import divider from "./images/pattern-divider-desktop.svg";
 
 function App() {
+  const [advice, setAdvice] = useState("Click a dice to get a random advice");
+  const [adviceID, setAdviceID] = useState("");
+
+  async function handleAdviceButton() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      setAdvice("Error! Failed to load data");
+      return;
+    }
+    const data = await response.json();
+
+    setAdviceID(data.slip.id);
+    setAdvice(data.slip.advice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className="advice-number">{`Advice #${adviceID}`}</p>
+      <p className="advice-quote">{advice}</p>
+      <img className="divider" src={divider} alt="divider pattern" />
+      <button className="btn-dice" onClick={handleAdviceButton}>
+        <img src={dice} alt="dice logo" />
+      </button>
     </div>
   );
 }
